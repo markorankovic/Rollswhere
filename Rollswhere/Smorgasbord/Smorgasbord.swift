371d16800d7e66a08600dcdf4b7bf2516b2df897
Marko Rankovic <marko@markorankovic.com>
2020-01-15 21:10:21 +0000

    dragging in sync + more ball damping

---
+++ Rollswhere/Smorgasbord/Smorgasbord.swift
@@ -0,0 +1,39 @@
+import SpriteKit
+import GameplayKit
+
+extension Sequence {
+    
+    public func filter<T>(_ type: T.Type) -> [T] {
+        return compactMap{ $0 as? T }
+    }
+    
+    func first<T>(_: T.Type) -> T? {
+        return first{ $0 is T } as? T
+    }
+    
+}
+
+extension CGPoint {
+    
+    public static func *(_ lhs: CGPoint, _ rhs: CGFloat) -> CGPoint {
+        return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    
+    public static func -(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    public static func +(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+}
+
+extension GKScene {
+    public func each<T: GKComponent>(_: T.Type) -> [T] {
+        return entities.compactMap{ $0.components.first(T.self) }
+    }
+}
+
+
+
