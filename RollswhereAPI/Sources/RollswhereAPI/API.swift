03370d6da9a1e9d5e8a62ab1a3f3ab931ec1452c
Marko Ranković <marko@markorankovic.com>
2020-11-02 04:49:03 +0000

    new features added to Requirements

--- RollswhereAPI/Sources/RollswhereAPI/API.swift
+++ RollswhereAPI/Sources/RollswhereAPI/API.swift
@@ -1,41 +1,28 @@
-public struct RollswhereAPI: RollswhereAPIProtocolRequirements {
-            
+public struct RollswhereAPI {
     public init() { }
-    
-    public func startWithLevel() -> Bool {
-        false
-    }
-    
-    public func shootBall() -> Bool {
-        false
-    }
-    
-    public func returnBall() -> Bool {
-        false
-    }
-    
-    public func nextLevel() -> Bool {
-        false
-    }
-    
-    public func returnToMainMenu() -> Bool {
-        false
-    }
-    
-    public func shootHook() -> Bool {
-        false
-    }
-    
-    public  func toggleActivateBall() -> Bool {
-        false
-    }
-    
-    public func zoomFeatures() -> Bool {
-        false
-    }
-    
-    public func showPowerLevel() -> Bool {
-        false
-    }
-            
+}
+
+extension RollswhereAPI: Requirements {
+    public func exitKeys() -> Bool                      { false }
+    public func interactiveMap() -> Bool                { false }
+    public func mainMenu() -> Bool                      { false }
+    public func levelSelection() -> Bool                { false }
+    public func programmaticLevelConstruction() -> Bool { false }
+    public func portals() -> Bool                       { false }
+    public func breaking() -> Bool                      { false }
+    public func iOSAndmacOSCompatibility() -> Bool      { false }
+    public func togglePause() -> Bool                   { false }
+    public func implementedLevelIdeas() -> Bool         { false }
+    public func autoGameSaveAndLoad() -> Bool           { false }
+    public func allSpecialsImplemented() -> Bool        { false }
+    public func draggableAndRotatingItems() -> Bool     { false }
+    public func startWithLevel() -> Bool                { false }
+    public func shootBall() -> Bool                     { false }
+    public func returnBall() -> Bool                    { false }
+    public func nextLevel() -> Bool                     { false }
+    public func returnToMainMenu() -> Bool              { false }
+    public func shootHook() -> Bool                     { false }
+    public func toggleActivateBall() -> Bool            { false }
+    public func zoomFeatures() -> Bool                  { false }
+    public func showPowerLevel() -> Bool                { false }
 }
--- RollswhereAPI/Sources/RollswhereAPI/RollswhereAPIProtocolRequirements.swift
+++ RollswhereAPI/Sources/RollswhereAPI/RollswhereAPIProtocolRequirements.swift
@@ -1,6 +1,6 @@
 public protocol Level { }
 
-public protocol RollswhereAPIProtocolRequirements {
+public protocol Requirements {
     func startWithLevel() -> Bool
     func shootBall() -> Bool
     func returnBall() -> Bool
@@ -10,4 +10,17 @@ public protocol RollswhereAPIProtocolRequirements {
     func toggleActivateBall() -> Bool
     func zoomFeatures() -> Bool
     func showPowerLevel() -> Bool
+    func programmaticLevelConstruction() -> Bool
+    func portals() -> Bool
+    func breaking() -> Bool
+    func iOSAndmacOSCompatibility() -> Bool
+    func togglePause() -> Bool
+    func implementedLevelIdeas() -> Bool
+    func autoGameSaveAndLoad() -> Bool
+    func allSpecialsImplemented() -> Bool
+    func draggableAndRotatingItems() -> Bool
+    func exitKeys() -> Bool
+    func interactiveMap() -> Bool
+    func mainMenu() -> Bool
+    func levelSelection() -> Bool
 }
